603907f969fcb7d7cda419b24633a150654399f4
pacozavala <paco.zavala@houston-inc.com>
2025-06-13 14:29:38 +0300

    [Release] 1.5: Add CORS support, health check endpoint and update todos route

--- project/server/app.js
+++ project/server/app.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 
 const todoappRouter = require('./controllers/todoapp')
 
-app.use('/', todoappRouter)
+app.use(cors())
+
+app.use('/api/todos', todoappRouter)
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
 module.exports = app
--- project/server/controllers/todoapp.js
+++ project/server/controllers/todoapp.js
@@ -1,9 +1,13 @@
 const todoappRouter = require('express').Router()
 
-const todo = { text: "I need to clean the house", status: "not-done" }
+const todos = [
+  { id: '01', text: 'I need to clean the house', status: 'not-done' },
+  { id: '02', text: 'Another to do', status: 'not-done' },
+]
 
-todoappRouter.get('/', async (req, res) => {
-  return res.status(201).send(todo)
+todoappRouter.get('/', (req, res) => {
+  console.log('GET request to /api/todos done succesfully')
+  res.status(201).send(todos)
 })
 
 module.exports = todoappRouter

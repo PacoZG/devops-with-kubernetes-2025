b47ce87f1d9007843b36a69b024d1014d2f15c48
pacozavala <paco.zavala@houston-inc.com>
2025-06-24 07:43:43 +0300

    [Release] 2.1: Refactor getPings.js: replace fetch with axios for improved error handling and response logging

--- pingpong-log-output/log_output/src/reader/services/getPings.js
+++ pingpong-log-output/log_output/src/reader/services/getPings.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 const getPings = async () => {
   const pingServerUrl = process.env.PING_SERVER_URL
   if (!pingServerUrl) throw new Error('PING_SERVER_URL is not set')
@@ -5,10 +7,21 @@ const getPings = async () => {
   console.log(`[READER]: Pingpong baseUrl ${pingServerUrl}`)
 
   try {
-    const response = await fetch(`${pingServerUrl}/ping`, { method: 'GET' })
+    const response = await axios({
+      method: 'GET',
+      url: `${pingServerUrl}/pings`,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    console.log('RESPONSE: ', response)
 
-    if (!response.ok) throw new Error(`[READER]: HTTP error! status: ${response.status}`)
-    const { pongs } = await response.json()
+    if (response.status !== 200) {
+      console.log('RESPONSE: ', response)
+      throw new Error(`[READER]: HTTP error! status: ${response.status}`)
+    }
+    console.log('RESPONSE DATA: ', response.data)
+    const { pongs } = response.data
 
     return pongs
   } catch (error) {

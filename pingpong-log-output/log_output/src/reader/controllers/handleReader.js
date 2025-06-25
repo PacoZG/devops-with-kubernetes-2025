99948957e789784498fdeadf198bf2c0c755bcc3
pacozavala <paco.zavala@houston-inc.com>
2025-06-25 10:09:03 +0300

    Enhance hashReaderRouter: include MESSAGE from appConfig.js and text from information.txt file and display file content

--- pingpong-log-output/log_output/src/reader/controllers/handleReader.js
+++ pingpong-log-output/log_output/src/reader/controllers/handleReader.js
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 import getHash from '../actions/getHash.js'
 import getPings from '../services/getPings.js'
+import { MESSAGE } from '../utils/appConfig.js'
+import getInformationText from '../actions/getInformationText.js'
 
 const hashReaderRouter = Router()
 
@@ -8,13 +10,13 @@ hashReaderRouter.get('/', async (req, res) => {
   console.log('[READER]: GET request to /api/strings done successfully')
   const hash = await getHash()
   const pings = await getPings()
+  const text = await getInformationText()
 
   res.status(200).send(`
-  <div style="background: black; color: white; padding: 16px; border-radius: 6px">
-    <div style="font-size: 24px">${hash}</div>
-    
-    <br/>
-    
+  <div style="background: black; color: darkorange; padding: 16px; border-radius: 6px">
+    <div style="font-size: 20px; margin-bottom: 5px">file content: ${text}</div>
+    <div style="font-size: 20px; margin-bottom: 5px">env variable: ${MESSAGE}</div>
+    <div style="font-size: 24px; margin-bottom: 5px">${hash}</div>
     <label style="font-size: 20px">Ping / Pongs: ${pings}</label>
   </div>`)
 })

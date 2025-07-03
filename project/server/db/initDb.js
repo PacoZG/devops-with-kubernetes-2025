2f50878d690b8b7c3571ce1f17cf15d4bccc9d69
pacozavala <paco.zavala@houston-inc.com>
2025-07-03 11:30:13 +0300

    [Release] 3.5: Refactor database configuration and initialization in initDb.js and appConfig.js

--- project/server/db/initDb.js
+++ project/server/db/initDb.js
@@ -1,43 +1,27 @@
 /* prettier-ignore */
 import pkg from 'pg';
 import {
-  POSTGRES_DATABASE,
   POSTGRES_HOST,
   POSTGRES_PASSWORD,
-  POSTGRES_USER
 } from '../utils/appConfig.js';
 
 const { Pool } = pkg;
 
 export const pool = new Pool({
+  user: 'postgres',
+  database: 'postgres',
   host: POSTGRES_HOST,
-  user: POSTGRES_USER,
   password: POSTGRES_PASSWORD,
-  database: POSTGRES_DATABASE,
   port: 5432,
 });
 
 export const initDb = async () => {
   await pool.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`);
   await pool.query(`
-      CREATE TABLE IF NOT EXISTS todos_table
-      (
-          id
-          UUID
-          PRIMARY
-          KEY
-          DEFAULT
-          uuid_generate_v4
-      (
-      ),
-          text VARCHAR
-      (
-          140
-      ) NOT NULL,
-          status VARCHAR
-      (
-          10
-      ) NOT NULL
-          );
+    CREATE TABLE IF NOT EXISTS todos_table (
+      id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+      text VARCHAR(140) NOT NULL,
+      status VARCHAR(10) NOT NULL
+    );
   `);
 };

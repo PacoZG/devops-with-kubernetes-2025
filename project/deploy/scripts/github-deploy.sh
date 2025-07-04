7c95942c6e073ea492733a9f8571ef9dbd403401
pacozavala <paco.zavala@houston-inc.com>
2025-07-04 12:12:19 +0300

    [Release] 3.7: Refactor deployment scripts and update GitHub Actions workflow for improved namespace handling and dynamic image setting

--- project/deploy/scripts/github-deploy.sh
+++ project/deploy/scripts/github-deploy.sh
@@ -6,12 +6,10 @@ YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color (reset)
 
-cd deploy
-
 printf "${BLUE}Running Kubernetes deployments script${NC}\n"
 
-printf "${YELLOW}Ensuring 'project' namespace exists...${NC}\n"
-kubectl get namespace project >/dev/null 2>&1 || kubectl apply -f kubernetes/namespace
+printf "${YELLOW}Ensuring '${NAMESPACE_NAME}' namespace exists...${NC}\n"
+kubectl get namespace "${NAMESPACE_NAME}" >/dev/null 2>&1 || kubectl create namespace "${NAMESPACE_NAME}"
 
 printf "${YELLOW}Creating Kubernetes volumes${NC}\n"
 kubectl apply -f kubernetes/volumes/gkepersistentvolumeclaim.yaml
@@ -19,15 +17,26 @@ kubectl apply -f kubernetes/volumes/gkepersistentvolumeclaim.yaml
 printf "${YELLOW}Checking for secret.yaml...${NC}\n"
 if [ ! -f kubernetes/base/secret.yaml ]; then
   printf "${GREEN}Creating secret.yaml file${NC}\n"
+  sops --version
   export SOPS_AGE_KEY_FILE=$(pwd)/key.txt
   sops --decrypt secret.enc.yaml > kubernetes/base/secret.yaml
 else
   printf "${YELLOW}kubernetes/base/secret.yaml already exists${NC}\n"
 fi
 
+printf "${YELLOW}Setting images via kustomize${NC}\n"
+cd kubernetes/base
+kustomize edit set namespace "${NAMESPACE_NAME}"
+kustomize edit set image CLIENT/IMAGE=${GCP_REGISTRY_PATH}/${CLIENT_IMAGE}:${IMAGE_TAG}
+kustomize edit set image SERVER/IMAGE=${GCP_REGISTRY_PATH}/${SERVER_IMAGE}:${IMAGE_TAG}
+
+
 printf "${GREEN}Deploying Kubernetes resources with ${NC}"
 printf "${YELLOW}kubectl apply -k kubernetes/overlays/dev${NC}\n"
-kubectl kustomize kubernetes/overlays/dev | kubectl apply -f -
+kustomize build . | kubectl apply -f -
+kubectl rollout status deployment ${CLIENT_IMAGE}
+kubectl rollout status deployment ${SERVER_IMAGE}
+kubectl get services -o wide
 if [ $? -ne 0 ]; then
   printf "${RED}Error: Failed to apply Kubernetes manifests${NC}\n"
   exit 1

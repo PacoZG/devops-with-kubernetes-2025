df9af000c7647fe7f2f721dac192ab5f79a0a973
pacozavala <paco.zavala@houston-inc.com>
2025-07-08 10:27:18 +0300

    Refine local deployment script and fix typos

--- project/deploy/scripts/deploy-dev.sh
+++ project/deploy/scripts/deploy-dev.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
@@ -11,26 +13,44 @@ cd deploy
 printf "${BLUE}Running Kubernetes deployments script${NC}\n"
 
 printf "${YELLOW}Ensuring 'project' namespace exists...${NC}\n"
-kubectl get namespace project >/dev/null 2>&1 || kubectl apply -f kubernetes/namespace
+kubectl get namespace project >/dev/null 2>&1 || kubectl apply -f ../kubernetes/namespace
 
-printf "${YELLOW}Creating Kubernetes volumes${NC}\n"
-kubectl apply -f kubernetes/volumes/gkepersistentvolumeclaim.yaml
+PVC_NAME="project-files-claim" # Ensure this matches the metadata.name in your persistentvolumeclaim.yaml
 
-printf "${YELLOW}Checking for secret.yaml...${NC}\n"
-if [ ! -f kubernetes/base/secret.yaml ]; then
-  printf "${GREEN}Creating secret.yaml file${NC}\n"
-  export SOPS_AGE_KEY_FILE=$(pwd)/key.txt
-  sops --decrypt secret.enc.yaml > kubernetes/base/secret.yaml
+printf "${YELLOW}Checking for Kubernetes volume: ${PVC_NAME} in namespace 'Project'${NC}\n"
+if ! kubectl get pvc "${PVC_NAME}" --ignore-not-found=true | grep -q "${PVC_NAME}"; then
+  printf "${YELLOW}Kubernetes volume ${PVC_NAME} not found. Creating...${NC}\n"
+  kubectl apply -f kubernetes/volumes/persistentvolumeclaim.yaml
+  kubectl apply -f kubernetes/volumes/persistentvolume.yaml
 else
-  printf "${YELLOW}kubernetes/base/secret.yaml already exists${NC}\n"
+  printf "${YELLOW}Kubernetes volume ${PVC_NAME} already exists in namespace 'Project'. Skipping creation.${NC}\n"
 fi
 
+printf "${YELLOW}Checking for secrets.yaml...${NC}\n"
+if [ ! -f kubernetes/base/secrets.yaml ]; then
+  printf "${GREEN}Creating secrets.yaml file${NC}\n"
+  export SOPS_AGE_KEY_FILE=$(pwd)/key.txt
+  sops --decrypt secrets.enc.yaml > kubernetes/base/secrets.yaml
+else
+  printf "${YELLOW}kubernetes/base/secrets.yaml already exists${NC}\n"
+fi
+
+cd kubernetes/base
+
+export DEPLOY_PATH=$(pwd)
+
+printf "$DEPLOY_PATH\n"
+
 printf "${GREEN}Deploying Kubernetes resources with ${NC}"
-printf "${YELLOW}kubectl apply -k kubernetes/overlays/dev${NC}\n"
-kubectl kustomize kubernetes/overlays/dev | kubectl apply -f -
+printf "${YELLOW}kubectl apply -k kubernetes/base${NC}\n"
+kustomize build . | kubectl apply -f -
 if [ $? -ne 0 ]; then
   printf "${RED}Error: Failed to apply Kubernetes manifests${NC}\n"
   exit 1
 fi
+
+kubectl rollout status deployment client-dep
+kubectl rollout status deployment server-dep
+kubectl get services -o wide
 
 printf "${GREEN}Deployment successfully completed${NC}\n"

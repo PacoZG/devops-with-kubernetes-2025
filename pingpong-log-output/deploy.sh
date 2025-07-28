c4c9c0e16bb2954799b0b74e2f26d51db05e7d06
pacozavala <paco.zavala@houston-inc.com>
2025-07-28 07:21:59 +0300

    [Release] 3.03: Introduce solution for exercise 3.3

--- pingpong-log-output/deploy.sh
+++ pingpong-log-output/deploy.sh
@@ -10,21 +10,27 @@ printf "${BLUE}Running Kubernetes deployments script${NC}\n"
 
 printf "${YELLOW}Ensuring 'exercises' namespace exists...${NC}\n"
 kubectl get namespace exercises >/dev/null 2>&1 || kubectl apply -f kubernetes/namespace
+kubectl config set-context --current --namespace=exercises
 
 printf "${YELLOW}Creating Kubernetes volumes${NC}\n"
-kubectl apply -f kubernetes/volumes/gkepersistentvolumeclaim.yaml
+kubectl apply -f kubernetes/volumes/gkepersistentvolumeclaim.yaml -n exercises
+
+printf "${YELLOW}Creating Kubernetes Gateway${NC}\n"
+kubectl apply -f kubernetes/gateway -n exercises
+
 
 printf "${YELLOW}Checking for secret.yaml...${NC}\n"
 if [ ! -f kubernetes/manifests/secret.yaml ]; then
   printf "${GREEN}Creating secret.yaml file${NC}\n"
   export SOPS_AGE_KEY_FILE=$(pwd)/key.txt
   sops --decrypt secret.enc.yaml > kubernetes/manifests/secret.yaml
+  kubectl apply -n secret.yaml -n exercises
 else
   printf "${YELLOW}manifests/secret.yaml already exists${NC}\n"
 fi
 
 printf "${GREEN}Deploying Kubernetes resources from manifests${NC}\n"
-kubectl apply -f kubernetes/manifests
+kubectl apply -f kubernetes/manifests -n exercises
 if [ $? -ne 0 ]; then
   printf "${RED}Error: Failed to apply Kubernetes manifests${NC}\n"
   exit 1
